6ec070db1eac730affda01c2b720739a11398008
will Golding <willgolding117@gmail.com>
2018-04-13 13:55:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,10 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element) {
+  var array = [];
+  array = [...element];
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return array.push(element);
+}
